b0f6a281828d21a000e95aaf3b0e5c8fdd4b9b15
yassermo97 <mhmd.yasser@gmail.com>
2022-03-23 19:34:38 +0200

    Changed to np arrays and others
    Set a MAX_RANGE constant to simulate physical limits.
    Dealt with unreachable nodes as leftovers.

--- pyswaro.py
+++ pyswaro.py
@@ -1,37 +1,35 @@
-from sre_constants import FAILURE
+from pprint import pprint
 import numpy as np
-import random
 
-ROWS, COLS = (8, 8)
+ROWS, COLS = (7, 7)
 MIN_INIT_BATTERY = 50
 MAX_INIT_BATTERY = 100
+MAX_RANGE = 5
 BATTERY_TOLLS = {
-    'COMMUNICATION': 0.05
+    'COMMUNICATION': 0.05,
+    'MOVEMENT': 0.5
 }
 TIME_TOLLS = {
     'SCAN': 4e-3,
     'TIMEOUT': 2,
-    'PER_RANGE': 1e-3
+    'PER_RANGE': 1e-3,
+    'LEFTOVER': 3e-3
 }
 
-LEADERS_COORDINATES = [(2, 0), (2, 5), (5, 5), (7, 1)]
+LEADERS_COORDINATES = [(3, 3)]
 terrain = [[]]
 visited = [[]]
-leader_count = [0] * len(LEADERS_COORDINATES)
+leader_tally = []
+leftovers = 0
 total_time_consumed = 0
 
 
-def init_terrain():
-    global terrain, visited
-    terrain = [[0] * COLS] * ROWS
-    visited = [[0] * COLS] * ROWS
-
-
-def init_charge_batteries():
-    global terrain
+def init():
+    global terrain, visited, leader_tally
     terrain = np.random.randint(
         MIN_INIT_BATTERY, MAX_INIT_BATTERY + 1, size=(ROWS, COLS))
-    terrain = terrain.tolist()
+    visited = np.zeros([ROWS, COLS])
+    leader_tally = np.zeros(len(LEADERS_COORDINATES))
 
 
 def set_leaders():
@@ -41,9 +39,10 @@ def set_leaders():
 
 
 def check_visited():
+    global visited
     for i in range(0, ROWS):
         for j in range(0, COLS):
-            if visited[i][j] == 0:
+            if visited[i][j] == 0 and (i, j) not in LEADERS_COORDINATES:
                 return False
     return True
 
@@ -63,7 +62,7 @@ def leader_scan(scan_range):
         if j+scan_range < COLS:
             if visited[i][j+scan_range] == 0:
                 visited[i][j+scan_range] = 1
-                leader_count[current_leader] += 1
+                leader_tally[current_leader] += 1
 
             drain_battery(i, j)  # Leader
             drain_battery(i, j+scan_range)  # Subordinate
@@ -76,7 +75,7 @@ def leader_scan(scan_range):
         if j-scan_range >= 0:
             if visited[i][j-scan_range] == 0:
                 visited[i][j-scan_range] = 1
-                leader_count[current_leader] += 1
+                leader_tally[current_leader] += 1
 
             drain_battery(i, j)
             drain_battery(i, j-scan_range)
@@ -89,7 +88,7 @@ def leader_scan(scan_range):
         if i-scan_range >= 0:
             if visited[i-scan_range][j] == 0:
                 visited[i-scan_range][j] = 1
-                leader_count[current_leader] += 1
+                leader_tally[current_leader] += 1
 
             drain_battery(i, j)
             drain_battery(i-scan_range, j)
@@ -102,7 +101,7 @@ def leader_scan(scan_range):
         if i+scan_range < ROWS:
             if visited[i+scan_range][j] == 0:
                 visited[i+scan_range][j] = 1
-                leader_count[current_leader] += 1
+                leader_tally[current_leader] += 1
 
             drain_battery(i, j)
             drain_battery(i+scan_range, j)
@@ -115,7 +114,7 @@ def leader_scan(scan_range):
         if i-scan_range >= 0 and j+scan_range < COLS:
             if visited[i-scan_range][j+scan_range] == 0:
                 visited[i-scan_range][j+scan_range] = 1
-                leader_count[current_leader] += 1
+                leader_tally[current_leader] += 1
 
             drain_battery(i, j)
             drain_battery(i-scan_range, j+scan_range)
@@ -128,7 +127,7 @@ def leader_scan(scan_range):
         if i-scan_range >= 0 and j-scan_range >= 0:
             if visited[i-scan_range][j-scan_range] == 0:
                 visited[i-scan_range][j-scan_range] = 1
-                leader_count[current_leader] += 1
+                leader_tally[current_leader] += 1
 
             drain_battery(i, j)
             drain_battery(i-scan_range, j-scan_range)
@@ -141,7 +140,7 @@ def leader_scan(scan_range):
         if i+scan_range < ROWS and j+scan_range < COLS:
             if visited[i+scan_range][j+scan_range] == 0:
                 visited[i+scan_range][j+scan_range] = 1
-                leader_count[current_leader] += 1
+                leader_tally[current_leader] += 1
 
             drain_battery(i, j)
             drain_battery(i+scan_range, j+scan_range)
@@ -154,7 +153,7 @@ def leader_scan(scan_range):
         if i+scan_range < ROWS and j-scan_range >= 0:
             if visited[i+scan_range][j-scan_range] == 0:
                 visited[i+scan_range][j-scan_range] = 1
-                leader_count[current_leader] += 1
+                leader_tally[current_leader] += 1
 
             drain_battery(i, j)
             drain_battery(i+scan_range, j-scan_range)
@@ -164,7 +163,7 @@ def leader_scan(scan_range):
             failures += 1
 
         # Calculate failure tolls for leader
-        for i in range(failures):
+        for failure in range(failures):
             drain_battery(i, j)
             total_time_consumed += TIME_TOLLS['SCAN'] + \
                 (TIME_TOLLS['PER_RANGE'] * scan_range)
@@ -174,10 +173,45 @@ def leader_scan(scan_range):
         current_leader += 1
 
 
-init_terrain()
-init_charge_batteries()
+def scan_leftovers():
+    global visited, total_time_consumed, leftovers
+    for i in range(0, ROWS):
+        for j in range(0, COLS):
+            if visited[i][j] == 0:
+                visited[i][j] = 1
+                leftovers += 1
+
+    for i, j in LEADERS_COORDINATES:
+        drain_battery(i, j, 'MOVEMENT')
+        total_time_consumed += TIME_TOLLS['LEFTOVER']
+
+
+def leader_count():
+    scan_range = 1
+    while check_visited() == False:
+        if scan_range == MAX_RANGE:
+            scan_leftovers()
+            break
+        leader_scan(scan_range)
+        scan_range += 1
+
+
+init()
 set_leaders()
-print(terrain)
-leader_scan(scan_range=1)
-print(leader_count)
-print(terrain)
+
+pprint('Terrain before:')
+pprint(terrain)
+
+leader_count()
+
+pprint('Terrain after:')
+pprint(terrain)
+
+pprint('Visited after:')
+pprint(visited)
+
+pprint('Counted:')
+pprint(np.sum(leader_tally) + leftovers)
+
+pprint(f'Total time consumed: {total_time_consumed} seconds')
+# TODO: Deal with idle time and depleted batteries
